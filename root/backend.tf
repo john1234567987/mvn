dcabc02017ffdd7696079600bf7ba1da47b9a8f4
john1234567987 <anigbojohn9@gmail.com>
2025-08-17 17:31:03 +0100

    Update backend.tf

--- root/backend.tf
+++ root/backend.tf
@@ -1,4 +1,10 @@
 terraform {
-  
+  backend "s3" {
+    bucket         = "todo-list-1987"   # Replace with your S3 bucket name
+    key            = "state/dev/terraform.tfstate"  # Path inside the bucket
+    region         = "eu-west-2"                   # AWS region
+    dynamodb_table = "terraform-lock"             # DynamoDB table for state locking
+    encrypt        = true                          # Encrypt state at rest in S3
+  }
 }
 
